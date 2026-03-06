4e4bc60cd830c8f7fa50f27f36740e13ca8bc065
lapshy <yano4kalapshina@gmail.com>
2026-03-06 22:01:27 +0300

    Update TaskAddCommand.java

--- cli/TaskAddCommand.java
+++ cli/TaskAddCommand.java
@@ -1,1 +1,45 @@
+package org.itmo.cli.commands;
 
+import org.itmo.cli.Command;
+import org.itmo.domain.*;
+import org.itmo.service.TaskService;
+import java.time.Instant;
+import java.util.Scanner;
+
+public class TaskAddCommand extends Command {
+    
+    public TaskAddCommand(TaskService taskService) {
+        super(taskService, "task_add", "Создать новую задачу с пошаговым вводом", 1);
+    }
+
+    @Override
+    public void execute(String[] args, Scanner scanner) {
+        System.out.println("--- Режим создания задачи ---");
+        
+        System.out.print("Введите текст задачи: ");
+        String text = scanner.nextLine();
+
+        TaskPriority priority = null;
+        while (priority == null) {
+            System.out.print("Введите приоритет (LOW, MEDIUM, HIGH): ");
+            try {
+                String input = scanner.nextLine().trim().toUpperCase();
+                priority = TaskPriority.valueOf(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Ошибка: Такого приоритета не существует. Попробуйте снова.");
+            }
+        }
+
+        Task newTask = new Task(
+            text, 
+            priority, 
+            TaskStatus.NEW, 
+            null, 
+            Instant.now(), 
+            "STUDENT_ITMO"
+        );
+
+        taskService.addTask(newTask);
+        System.out.println("Задача успешно создана! Присвоен ID: " + newTask.getId());
+    }
+}
