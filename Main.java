50c5a844a2c5a2c0a49a8faace05a27422fb55b4
lapshy <yano4kalapshina@gmail.com>
2026-03-06 22:26:08 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+package org.itmo;
+
+import org.itmo.cli.ConsoleHandler;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.start();
+    }
+}
