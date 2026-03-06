83f2e95206ab66eca138ad3f7d311718f5dbcd43
lapshy <yano4kalapshina@gmail.com>
2026-03-06 21:58:04 +0300

    Update Command.java

--- cli/Command.java
+++ cli/Command.java
@@ -1,1 +1,29 @@
+package org.itmo.cli;
 
+import org.itmo.service.TaskService;
+import java.util.Scanner;
+
+public abstract class Command {
+    protected final TaskService taskService;
+    private final String name;
+    private final String description;
+    private final int minArgs; 
+    public Command(TaskService taskService, String name, String description, int minArgs) {
+        this.taskService = taskService;
+        this.name = name;
+        this.description = description;
+        this.minArgs = minArgs;
+    }
+
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+
+    public void validateArgs(String[] args) {
+        if (args.length < minArgs) {
+            throw new IllegalArgumentException("Ошибка: Команде '" + name + "' недостаточно данных.");
+        }
+    }
+
+    // Основной метод, который переопределяют дочерние классы
+    public abstract void execute(String[] args, Scanner scanner);
+}
